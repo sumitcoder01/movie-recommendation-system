0bc97b85fad3d50e32f3c327c5781956776fab0b
sumitcoder01 <135352466+sumitcoder01@users.noreply.github.com>
2024-04-29 17:40:59 +0530

    fix: fix image sizes

--- frontend/app.py
+++ frontend/app.py
@@ -11,10 +11,13 @@ cosine_matrix = pd.read_pickle('cosine_matrix.pkl')
 
 def recommend(series_title):
     recommend_movie = []
-    index = movies[movies['Series_Title'] == series_title].index[0]
-    movies_list = sorted(list(enumerate(cosine_matrix[index])), reverse=True, key=lambda x: x[1])[:5]
-    for item in movies_list:
-        recommend_movie.append([movies.iloc[item[0]]['Series_Title'],movies.iloc[item[0]]['Poster_Link']])
+    try:    
+        index = movies[movies['Series_Title'] == series_title].index[0]
+        movies_list = sorted(list(enumerate(cosine_matrix[index])), reverse=True, key=lambda x: x[1])[:5]
+        for item in movies_list:
+            recommend_movie.append([movies.iloc[item[0]]['Series_Title'],movies.iloc[item[0]]['Poster_Link']])
+    except Exception as e:
+        st.warning('Movie not found', icon="⚠️")
     return recommend_movie   
  
 def load_image(image_url):
@@ -39,23 +42,26 @@ selected_movie = st.selectbox(
 if st.button('Recommend'):
     recommend_movie = recommend(selected_movie)
     st.subheader('Recommended movies')
-    col1, col2 = st.columns(2, gap="medium")
+    if len(recommend_movie):
+        col1, col2 = st.columns(2, gap="medium")
 
-    col1.header(recommend_movie[0][0])
-    col1.image(load_image(recommend_movie[0][1]), width=150, caption='Image not found' if recommend_movie[0][1] is None else None)
+        col1.header(recommend_movie[0][0])
+        col1.image(load_image(recommend_movie[0][1]), width=100, caption='Image not found' if recommend_movie[0][1] is None else None)
 
-    col2.header(recommend_movie[1][0])
-    col2.image(load_image(recommend_movie[1][1]), width=150, caption='Image not found' if recommend_movie[1][1] is None else None)
+        col2.header(recommend_movie[1][0])
+        col2.image(load_image(recommend_movie[1][1]), width=100, caption='Image not found' if recommend_movie[1][1] is None else None)
 
-    col1, col2 = st.columns(2, gap="medium")
+        col1, col2 = st.columns(2, gap="medium")
 
-    col1.header(recommend_movie[2][0])
-    col1.image(load_image(recommend_movie[2][1]), width=150, caption='Image not found' if recommend_movie[2][1] is None else None)
+        col1.header(recommend_movie[2][0])
+        col1.image(load_image(recommend_movie[2][1]), width=100, caption='Image not found' if recommend_movie[2][1] is None else None)
 
-    col2.header(recommend_movie[3][0])
-    col2.image(load_image(recommend_movie[3][1]), width=150, caption='Image not found' if recommend_movie[3][1] is None else None)
+        col2.header(recommend_movie[3][0])
+        col2.image(load_image(recommend_movie[3][1]), width=100, caption='Image not found' if recommend_movie[3][1] is None else None)
 
-    st.header(recommend_movie[4][0])
-    st.image(load_image(recommend_movie[4][1]), width=150, caption='Image not found' if recommend_movie[4][1] is None else None)
+        st.header(recommend_movie[4][0])
+        st.image(load_image(recommend_movie[4][1]), width=100, caption='Image not found' if recommend_movie[4][1] is None else None)
+    else:
+        st.header("Movie not found!")
 else:
     st.subheader('Select a movie for recommendation')
